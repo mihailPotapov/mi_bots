45d0d7d5f820b735424ca7ca4b0779264c4449c1
mihailPotapov <mihail.potapov.2019@mail.ru>
2024-01-26 22:02:03 +0300

    улучшение: добавлено система ролей

--- bot/api_gpt.py
+++ bot/api_gpt.py
@@ -1,95 +1,154 @@
 import telebot
-import psycopg2
 from telebot import types
-from data_manager import TOKEN
 from openai import OpenAI
+from data_manager import TOKEN
+from db_utils import (
+    user_exists,
+    add_user_to_db,
+    get_current_role,
+    update_chat_role,
+    get_db_connection,
+    get_role_name
+)
 
+
+bot = telebot.TeleBot(TOKEN)
 client = OpenAI(api_key='')
 
 bot = telebot.TeleBot(TOKEN)
-gpt_chat_enabled = {}  # Используем словарь для хранения состояния чата
-
-# Конфигурация подключения к базе данных
-db_config = {
-    "host": "localhost",
-    "database": "bot_gpt",
-    "user": "postgres",
-    "password": "123"
-}
+active_chats = {}
+WELCOME_MESSAGE = (
+    "Что бы начать диалог нажмите на кнопку '/gpt_chat'\n"
+    "Что бы выбрать роль нажмите '/gpt_roles'\n"
+    "Что бы проверить текущую роль нажмите '/current_role'\n"
+    "Что бы завершить диалог нажмите на кнопку '/stop'"
+)
 
 
-def get_db_connection():
-    conn = psycopg2.connect(**db_config)
-    return conn
-
-
-def user_exists(nickname_user):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT id FROM users WHERE nickname_user = %s", (nickname_user,))
-    user = cursor.fetchone()
-    cursor.close()
-    conn.close()
-    return user is not None
-
-
-# Функция для добавления пользователя в БД
-def add_user_to_db(name_user, nickname_user, id_chat):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (name_user, nickname_user, id_chat) VALUES (%s, %s, %s)",
-                   (name_user, nickname_user, id_chat))
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-
+# Обработчики команд бота
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    user = message.from_user
     hi = open('image/Hi.webp', 'rb')
+    user = message.from_user
     if user_exists(user.username):
         bot.send_sticker(message.chat.id, hi)
         bot.send_message(message.chat.id, "Рад снова вас видеть!", reply_markup=start_menu())
-        print("новый пользователь", user.first_name, user.username, message.chat.id)
+        bot.send_message(message.chat.id, WELCOME_MESSAGE)
+        # bot.send_message(message.chat.id, "Что бы выбрать роль нажмите 'gpt_roles'")
+        # bot.send_message(message.chat.id, "Что бы проверить текущую роль нажмите 'current_role'")
+        # bot.send_message(message.chat.id, "Что бы завершить диалог нажмите на кнопку 'stop'")
     else:
         bot.send_sticker(message.chat.id, hi)
         add_user_to_db(user.first_name, user.username, message.chat.id)
         bot.send_message(message.chat.id, "Рад видеть новое лицо!", reply_markup=start_menu())
+        bot.send_message(message.chat.id, WELCOME_MESSAGE)
+        print("новый пользователь", user.first_name, user.username, message.chat.id)
 
 
-# Функция для включения режима GPT чата
 @bot.message_handler(commands=['gpt_chat'])
 def enable_gpt_chat(message):
     chat_id = message.chat.id
-    gpt_chat_enabled[chat_id] = True
+    active_chats[chat_id] = True
+
+    # Проверяем, есть ли уже запись в chat_roles для этого чата
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id_chat FROM chat_roles WHERE id_chat = %s", (chat_id,))
+    if not cursor.fetchone():
+        # Если нет, создаем новую запись с начальной ролью (например, id роли 1)
+        cursor.execute("INSERT INTO chat_roles (id_chat, id_roles) VALUES (%s, %s)", (chat_id, 1))
+        conn.commit()
+    cursor.close()
+    conn.close()
+
     bot.send_message(chat_id, "Режим GPT чата включен.")
 
 
-# Функция для отключения режима GPT чата
-@bot.message_handler(commands=['back', 'stop'])
+@bot.message_handler(commands=['stop'])
 def disable_gpt_chat(message):
+    active_chats.pop(message.chat.id, None)
+    bot.send_message(message.chat.id, "Режим GPT чата отключен.")
+
+
+@bot.message_handler(commands=['gpt_roles'])
+def list_roles(message):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id_roles, name_roles FROM roles")
+    roles = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    markup = types.InlineKeyboardMarkup()
+    for role in roles:
+        button = types.InlineKeyboardButton(role[1], callback_data=f"setrole_{role[0]}")
+        markup.add(button)
+
+    bot.send_message(message.chat.id, "Вот текущие доступные роли:", reply_markup=markup)
+
+
+@bot.message_handler(commands=['current_role'])
+def current_role(message):
     chat_id = message.chat.id
-    gpt_chat_enabled[chat_id] = False
-    bot.send_message(chat_id, "Режим GPT чата отключен.")
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    try:
+        # Запрос для получения текущей роли для чата
+        cursor.execute(
+            "SELECT r.name_roles FROM chat_roles cr "
+            "JOIN roles r ON cr.id_roles = r.id_roles "
+            "WHERE cr.id_chat = %s", (chat_id,))
+        role = cursor.fetchone()
+
+        # Отладочная печать результата запроса
+        print(f"Результат запроса для чата {chat_id}: {role}")
+
+        if role:
+            bot.send_message(chat_id, f"Текущая роль: {role[0]}")
+        else:
+            bot.send_message(chat_id, "Роль для данного чата не установлена.")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+    finally:
+        cursor.close()
+        conn.close()
 
 
-# Функция для обработки входящих сообщений
+# Функция обработки нажатия на кнопку inline-клавиатуры
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.message:
+        if call.data.startswith('setrole_'):
+            role_id = int(call.data.split('_')[1])
+            chat_id = call.message.chat.id
+
+            # Обновление роли в БД
+            update_chat_role(chat_id, role_id)
+
+            # Получение названия новой роли
+            role_name = get_role_name(role_id)
+
+            bot.answer_callback_query(call.id, f"Роль успешно сменена на '{role_name}'.")
+            bot.send_message(chat_id, f"Роль успешно сменена на '{role_name}'.")
+
+
 @bot.message_handler(func=lambda message: True)
 def gpt(message):
-    chat_id = message.chat.id
-    if gpt_chat_enabled.get(chat_id, False):
+    if message.chat.id in active_chats:
         prompt = message.text
         msg = bot.send_message(message.chat.id, 'Сообщение принято. Ждем ответа..')
+        role = get_current_role(message.chat.id)
+        system_message = f"Ты {role}, помощник" if role else "Ты помощник"
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
+            model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "Ты русский, научный помощник"},
+                {"role": "system", "content": system_message},
                 {"role": "user", "content": prompt}
             ]
         )
-        gpt_text = response.choices[0].message.content
         tokens = response.usage.total_tokens
+        gpt_text = response.choices[0].message.content
         bot.delete_message(message.chat.id, msg.message_id)
         bot.send_message(message.chat.id, gpt_text)
         bot.send_message(message.chat.id, f"потрачено следующее количество токенов: {tokens}")
@@ -100,13 +159,16 @@ def gpt(message):
         bot.reply_to(message, "режим GPT отключен")
 
 
+# Вспомогательные функции
 def start_menu():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item_chat_gpt = types.KeyboardButton("/gpt_chat")
     item2 = types.KeyboardButton("/start")
     item3 = types.KeyboardButton("расскажи анекдот про программиста")
+    item4 = types.KeyboardButton("/current_role")
     item5 = types.KeyboardButton("/stop")
-    markup.add(item_chat_gpt, item2, item5, item3)
+    item6 = types.KeyboardButton("/gpt_roles")
+    markup.add(item_chat_gpt, item2, item5, item3, item6, item4)
     return markup
 
 
---
+++ bot/db_utils.py
@@ -0,0 +1,84 @@
+import psycopg2
+# Конфигурация подключения к базе данных
+db_config = {
+    "host": "localhost",
+    "database": "bot_gpt",
+    "user": "postgres",
+    "password": "123"
+}
+
+
+# Функции для работы с базой данных
+def get_db_connection():
+    conn = psycopg2.connect(**db_config)
+    return conn
+
+
+def user_exists(nickname_user):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM users WHERE nickname_user = %s", (nickname_user,))
+    user = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return user is not None
+
+
+def add_user_to_db(name_user, nickname_user, id_chat):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (name_user, nickname_user, id_chat) VALUES (%s, %s, %s)",
+                   (name_user, nickname_user, id_chat))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def set_user_role(chat_id, role_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE chat_roles SET id_roles = %s WHERE id_chat = %s",
+                   (role_id, chat_id))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def get_current_role(chat_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT r.name_roles FROM chat_roles cr JOIN roles r ON cr.id_roles = r.id_roles WHERE cr.id_chat = %s", (chat_id,))
+    role_name = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return role_name[0] if role_name else None
+
+
+def update_chat_role(chat_id, role_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    try:
+        # Проверяем, существует ли уже запись для данного чата
+        cursor.execute("SELECT id FROM chat_roles WHERE id_chat = %s", (chat_id,))
+        if cursor.fetchone():
+            # Обновляем существующую запись
+            cursor.execute("UPDATE chat_roles SET id_roles = %s WHERE id_chat = %s", (role_id, chat_id))
+        else:
+            # Создаем новую запись
+            cursor.execute("INSERT INTO chat_roles (id_chat, id_roles) VALUES (%s, %s)", (chat_id, role_id))
+
+        conn.commit()
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+    finally:
+        cursor.close()
+        conn.close()
+
+def get_role_name(role_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT name_roles FROM roles WHERE id_roles = %s", (role_id,))
+    role_name = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return role_name[0] if role_name else None
